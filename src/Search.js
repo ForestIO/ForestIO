246c6c14ca05f72abfb8c63deff5fabe88792a42
Luke <luke.michals@gmail.com>
2017-07-26 12:30:48 -0700

    basic search component added

---
+++ src/Search.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+  render() {
+
+    console.log('search props --> ', this.props)
+    
+    // const treeSearchHandler = this.props.treeSearchHandler;
+    const searchResults = this.props.searchResults;
+    
+    let resultsArray = [];
+    for(let i=0;i<searchResults.length;i++){
+      let searchstyler=[{"background":"rgba(0,0,0,0.2)"},{"background":"rgba(0,0,0,0.0)"} ]
+      resultsArray.push(<div style = {searchstyler[i%2]} id={searchResults[i]._id} key={i} className="searchresult">{searchResults[i].name}</div>)
+    }
+
+    return (
+      <div id="searchcontainer">
+        <form onSubmit={this.props.handleSearchSubmit}>
+          <label>
+            Tree Name:
+            <input type="text" onChange={this.props.handleSearchChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {resultsArray}
+      </div>
+    )
+  }
+}
+
+export default Search;
